580a5eaa842f0f86734f69da70f4b90a152e241c
Kaquaoify <gregory.gavillet@gmail.com>
2025-10-11 22:16:14 +0200

    1

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from .blueprints.legacy import bp as legacy_bp
 
 def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app = Flask(__name__, template_folder="templates", static_folder="static")
     app.register_blueprint(legacy_bp)
     return app
